b8dd070dbcfe87e45e12b3b72a10f981316695c9
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-11-08 16:11:17 +0530

    implementing ExpenseForm

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -6,7 +6,53 @@ function ExpenseForm() {
   const [amount, setAmount] = useState("");
   const [category, setCategory] = useState("Food");
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!title || !amount) return;
+
+    const newExpense = {
+      id: Date.now(),
+      title,
+      amount: parseFloat(amount),
+      category,
+      date: new Date().toLocaleDateString(),
+    };
+
+    addExpense(newExpense);
+    setTitle("");
+    setAmount("");
+};
+
+return (
+  <form className="expense-form" onSubmit={handleSubmit}>
+    <input
+      type="text"
+      placeholder="Expense title..."
+      value={title}
+      onChange={(e) => setTitle(e,EventTarget.value)}
+    />
+    <input
+      type="number"
+      placeholder="Amount"
+      value={ amount }
+      onChange={(e) => setAmount(e.target.value)}
+    />
+    <select value={category} onChange={(e) => setCategory(e.target.value)}>
+      <option>Food</option>
+      <option>Travel</option>
+      <option>Shopping</option>
+      <option>Other</option>
+    
+    </select> 
+    <button type="submit">
+      Add
+    </button>
+  </form>
+);
+
 }
 
 export default ExpenseForm;
+
+
+// end code
